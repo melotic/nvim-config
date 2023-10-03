e305c99e797578e13f0f6946e0e8d38605e20a7b
Justin Perez <justinmp215@gmail.com>
2023-10-02 22:15:21 -0700

    feat: add more lang packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,11 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.cs" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
 }
