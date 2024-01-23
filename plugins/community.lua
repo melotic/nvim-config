2f8a0436b6ae7f239acefaa7c6067acbc7fde806
Justin Perez <justinmp215@gmail.com>
2024-01-22 20:57:09 -0600

    feat: add cpp pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.cs" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
